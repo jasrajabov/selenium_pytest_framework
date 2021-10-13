4710f8baf4d384359d60eb647b0a5b012df5baa1
Jas <jas.rajabov@gmail.com>
2021-10-12 21:00:16 -0400

    requirements

--- conftest.py
+++ conftest.py
@@ -18,7 +18,7 @@ def browser(config):
     opts.headless = config['chrome_options'].getboolean('headless')
     driver = config['browser']['browser']
     if driver == 'chrome':
-        _browser = webdriver.Chrome(options=opts)
+        _browser = webdriver.Chrome(options=opts, executable_path='/usr/local/bin/chromedriver')
     # Return the WebDriver instance for the setup
     yield _browser
     # Quit the WebDriver instance for the cleanup
